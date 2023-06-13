50b7c17257fe949365a8e8de95ccc962ad08d348
hakjunjoo <hakjun0918@nate.com>
2023-06-13 19:23:22 +0900

    취소 기능 구현
    기존의 주문 데이터를 저장했던 list가 3개 였는데 list를 2개로 줄여서 데이터를 저장함
    고객은 자신이 원하는 개수만큼 주문을 취소할 수 있도록 기능 구현

--- kiosk/Kiosk2.java
+++ kiosk/Kiosk2.java
@@ -1,5 +1,6 @@
 package kiosk;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Kiosk2 {
@@ -9,58 +10,64 @@ public class Kiosk2 {
 
     public static void kioskDisplay() {
         boolean start = true;
-        while(start) {
+        while (start) {
 //            MenuList.menuData(); //메뉴 초기화
             System.out.println("================================================\n");
             System.out.println("\"SHAKESHACK BURGER 에 오신걸 환영합니다.\"");
             System.out.println("원하시는 작업을 선택해주세요.");
             System.out.println("1. 메뉴     2. 관리자 메뉴     3. 시스템 종료");
             System.out.print("\n여기에 번호를 입력하세요 => ");
-            int menuClick = sc.nextInt();
+
+            String menuClick = sc.nextLine();
             switch (menuClick) {
-                case 1 :  // 메뉴 주문
+                case "1":  // 메뉴 주문
                     menuOrder(); // 메뉴 리스트 출력
                     break;
-                case 2 :  // 관리자 메뉴
+                case "2":  // 관리자 메뉴
                     break;
-                case 3 :  //시스템 종료
+                case "3":  //시스템 종료
                     System.out.println("프로그램을 종료합니다.");
                     System.out.println("감사합니다.");
                     start = false; //반복문을 벗어나서 프로그램이 종료된다.
                     break;
-                default :
+                default:
                     System.out.println("잘못된 입력입니다. 다시 입력해주세요.");
                     break;
             } // switch
+
+
         } // while
     } // kioskDisplay
 
     private static void menuOrder() { // 메뉴 주문
-        Loop : while (true) { // 초기 메뉴 화면을 위해 이름 붙은 반복문을 활용
+        Loop:
+        while (true) { // 초기 메뉴 화면을 위해 이름 붙은 반복문을 활용
             System.out.println("\n================================================\n");
             System.out.println("메뉴를 선택하셨습니다.");
             System.out.println("원하시는 작업을 선택해주세요.");
             System.out.println("1. 메뉴 주문     2. 장바구니 조회     3. 주문 취소     4. 초기 메뉴");
 
             System.out.print("\n여기에 번호를 입력하세요 => ");
-            int menuSelect = sc.nextInt();
+
+            String menuSelect = sc.nextLine();
             switch (menuSelect) {
-                case 1 :
+                case "1":
                     MenuList.menuData(); //메뉴 초기화
                     mainMenu(); // 메뉴 주문
                     break;
-                case 2 :
+                case "2":
                     orderList(); // 장바구니 조회
                     break;
-                case 3 :
+                case "3":
                     orderCancel(); // 주문 취소
                     break;
-                case 4 :
+                case "4":
                     break Loop; //초기 메뉴로 돌아가기
-                default :
+                default:
                     System.out.println("잘못된 입력입니다. 번호를 다시 입력해주세요.");
-                    break;
+
             } //switch
+
         } //while
     } //menuSelect
 
@@ -78,7 +85,7 @@ public class Kiosk2 {
         while (true) {
             System.out.print("\n여기에 번호를 입력하세요 => ");
             int menuSelect = sc.nextInt();
-            if(1 <= menuSelect && menuSelect <= 4) {
+            if (1 <= menuSelect && menuSelect <= 4) {
                 menuSelect(menuSelect);
                 break;
             } else {
@@ -88,9 +95,9 @@ public class Kiosk2 {
     } //mainMenu
 
     private static void menuSelect(int menuSelect) {
-        int i=0;
+        int i = 0;
         switch (menuSelect) {
-            case 1 : {
+            case 1: {
                 System.out.println("\n================================================\n");
                 System.out.println("[ Burgers Menu ]");
                 for (Product b : MenuList.getBurgerArr()) {
@@ -100,7 +107,7 @@ public class Kiosk2 {
                 System.out.print("\n여기에 번호를 입력하세요 => ");
                 break;
             }
-            case 2 : {
+            case 2: {
                 System.out.println("\n================================================\n");
                 System.out.println("[ FrozenCustard Menu ]");
                 for (Product b : MenuList.getFrozenCustardArr()) {
@@ -110,7 +117,7 @@ public class Kiosk2 {
                 System.out.print("\n여기에 번호를 입력하세요 => ");
                 break;
             }
-            case 3 : {
+            case 3: {
                 System.out.println("\n================================================\n");
                 System.out.println("[ Drink Menu ]");
                 for (Product b : MenuList.getDrinkArr()) {
@@ -120,7 +127,7 @@ public class Kiosk2 {
                 System.out.print("\n여기에 번호를 입력하세요 => ");
                 break;
             }
-            case 4 : {
+            case 4: {
                 System.out.println("\n================================================\n");
                 System.out.println("[ Beer Menu ]");
                 for (Product b : MenuList.getBeerArr()) {
@@ -135,7 +142,7 @@ public class Kiosk2 {
         //while 추가해야할듯
         int detailMenuSelect = sc.nextInt();
 
-        if(menuSelect == 1) {
+        if (menuSelect == 1) {
             addOptions(menuSelect, detailMenuSelect);
         } else {
             wishList(menuSelect, detailMenuSelect);
@@ -145,14 +152,14 @@ public class Kiosk2 {
     //burgers 추가 옵션
     private static void addOptions(int menuSelect, int detailMenuSelect) {
         System.out.println("\n================================================\n");
-        System.out.println(products[menuSelect-1][detailMenuSelect-1].toString());
+        System.out.println(products[menuSelect - 1][detailMenuSelect - 1].toString());
         System.out.println("옵션을 추가하시겠습니까?");
         System.out.println("1. 사이즈 업( + W 3.0 )     2. 추가 안함");
         System.out.print("\n여기에 번호를 입력하세요 => ");
 
         int options = sc.nextInt();
 
-        if(options == 1) {
+        if (options == 1) {
             sizeUp(menuSelect, detailMenuSelect);
             wishList(menuSelect, detailMenuSelect);
         } else if (options == 2) {
@@ -163,13 +170,13 @@ public class Kiosk2 {
     } //addOptions
 
     private static void sizeUp(int menuSelect, int detailMenuSelect) {
-        products[menuSelect-1][detailMenuSelect-1].setName("(사이즈업)"+products[menuSelect-1][detailMenuSelect-1].getName()); // 메뉴이름 (사이즈업)Burgers
-        products[menuSelect-1][detailMenuSelect-1].setPrice(products[menuSelect-1][detailMenuSelect-1].getPrice()+3.0); // 메뉴 가격 +3.0
+        products[menuSelect - 1][detailMenuSelect - 1].setName("(사이즈업)" + products[menuSelect - 1][detailMenuSelect - 1].getName()); // 메뉴이름 (사이즈업)Burgers
+        products[menuSelect - 1][detailMenuSelect - 1].setPrice(products[menuSelect - 1][detailMenuSelect - 1].getPrice() + 3.0); // 메뉴 가격 +3.0
     } //sizeUP
 
     private static void wishList(int menuSelect, int detailMenuSelect) {
         System.out.println("\n================================================\n");
-        System.out.println("\"" + products[menuSelect-1][detailMenuSelect-1].toString() + "\"");
+        System.out.println("\"" + products[menuSelect - 1][detailMenuSelect - 1].toString() + "\"");
         System.out.println("위 메뉴를 장바구니에 추가하시겠습니까?");
         System.out.println("1. 확인        2. 취소");
         System.out.print("\n여기에 번호를 입력하세요 => ");
@@ -183,11 +190,11 @@ public class Kiosk2 {
             System.out.println("취소되었습니다.");
             MenuList.menuData(); //바뀌었던 메뉴이름 초기화
         } else {
-            Order.getTotalWish().add(products[menuSelect-1][detailMenuSelect-1]);
+            Order.getTotalWish().add(products[menuSelect - 1][detailMenuSelect - 1]);
             boolean exists = false;
             //기존 장바구니에 상품이 있는 경우
-            for(int i=0; i<Order.getCntWish().size(); i++) {
-                if (Order.getCntWish().get(i).toString().equals(Kiosk2.products[menuSelect-1][detailMenuSelect-1].toString())) {
+            for (int i = 0; i < Order.getCntWish().size(); i++) {
+                if (Order.getCntWish().get(i).toString().equals(Kiosk2.products[menuSelect - 1][detailMenuSelect - 1].toString())) {
                     exists = true;
                     Product existingProduct = Order.getCntWish().get(i);
                     existingProduct.setCount(existingProduct.getCount() + 1); // 상품의 카운트 +1
@@ -196,16 +203,16 @@ public class Kiosk2 {
                 }
             }
             //기존 장바구니에 상품이 없는 경우
-            if(!exists) {
-                Order.getCntWish().add(products[menuSelect-1][detailMenuSelect-1]);
+            if (!exists) {
+                Order.getCntWish().add(products[menuSelect - 1][detailMenuSelect - 1]);
                 System.out.println("\n================================================\n");
-                System.out.println(products[menuSelect-1][detailMenuSelect-1].getName().strip() + " 가 장바구니에 추가되었습니다.");
+                System.out.println(products[menuSelect - 1][detailMenuSelect - 1].getName().strip() + " 가 장바구니에 추가되었습니다.");
             }
         }
     } //putInBasket
 
     private static void orderList() {
-        if(Order.getCntWish().size() == 0) {
+        if (Order.getCntWish().size() == 0) {
             System.out.println("\n================================================\n");
             System.out.println("현재 장바구니가 비어있습니다.");
             System.out.println("초기화면으로 돌아갑니다.");
@@ -222,10 +229,10 @@ public class Kiosk2 {
             System.out.println("\n위와 같이 주문하시겠습니까?");
             System.out.println("\n1. 주문      2. 메뉴로 돌아가기");
 
-            while(true) {
+            while (true) {
                 System.out.print("\n여기에 번호를 입력하세요 => ");
                 int finalOrderComplete = sc.nextInt();
-                if(finalOrderComplete == 1) {
+                if (finalOrderComplete == 1) {
                     orderComplete();
                     break;
                 } else if (finalOrderComplete == 2) {
@@ -251,13 +258,13 @@ public class Kiosk2 {
         Order.getCntWish().clear();
         System.out.println("\n================================================\n");
         System.out.println("주문이 완료되었습니다!");
-        System.out.println("대기번호는 [ " + (cnt++) +" ] 번 입니다.");
+        System.out.println("대기번호는 [ " + (cnt++) + " ] 번 입니다.");
         System.out.println("(3초 후 메뉴판으로 돌아갑니다.) \n");
         delay(3000);
     } //orderComplete
 
     private static void orderCancel() {
-        if(Order.getCntWish().size() == 0) {
+        if (Order.getCntWish().size() == 0) {
             System.out.println("\n================================================\n");
             System.out.println("현재 장바구니가 비어있어서 취소할 주문이 없습니다.");
             System.out.println("초기화면으로 돌아갑니다.\n");
@@ -267,16 +274,16 @@ public class Kiosk2 {
             System.out.println("현재 까지의 주문 목록은 다음과 같습니다.");
             System.out.println("[ Orders ]");
             for (int j = 0; j < Order.getCntWish().size(); j++) {
-                System.out.println((j+1)+ ". " + Order.getCntWish().get(j).toString_cnt());
+                System.out.println((j + 1) + ". " + Order.getCntWish().get(j).toString_cnt());
             }
 
             System.out.println("\n 취소할 주문의 번호를 입력해주세요.");
             System.out.print("여기에 번호를 입력하세요 => ");
             int orderCancelNumber = sc.nextInt();
 
-            if(Order.getCntWish().get(orderCancelNumber-1).getCount() == 1) { // 주문 개수가 1개인 경우는 list에서 원소를 remove한다.
-                for(int i = 0; i < Order.getCntWish().size(); i++) {
-                    if(orderCancelNumber == i+1) {
+            if (Order.getCntWish().get(orderCancelNumber - 1).getCount() == 1) { // 주문 개수가 1개인 경우는 list에서 원소를 remove한다.
+                for (int i = 0; i < Order.getCntWish().size(); i++) {
+                    if (orderCancelNumber == i + 1) {
                         System.out.println(Order.getCntWish().get(i).toString_cnt());
                         System.out.println("해당 주문을 취소했습니다.");
                         Order.getCntWish().remove(i);
@@ -284,10 +291,20 @@ public class Kiosk2 {
                     }
                 }
             } else { // 주문 개수가 2개 이상인 경우는 list에서 cnt를 줄인다.
-                System.out.println(Order.getCntWish().get(orderCancelNumber-1).toString_cnt());
+                System.out.println(Order.getCntWish().get(orderCancelNumber - 1).toString_cnt());
                 System.out.println("몇 개를 취소하시겠습니까?");
+                System.out.print("여기에 개수를 입력해주세요. =>");
+                int CancelNumber = sc.nextInt();
 
+                if (Order.getCntWish().get(orderCancelNumber - 1).getCount() > CancelNumber && CancelNumber >= 0) {
+                    Order.getCntWish().get(orderCancelNumber - 1).setCount(Order.getCntWish().get(orderCancelNumber - 1).getCount() - CancelNumber);
+                } else if (Order.getCntWish().get(orderCancelNumber - 1).getCount() == CancelNumber) {
+                    Order.getCntWish().remove(orderCancelNumber - 1);
+                } else {
+                    System.out.println("잘못된 입력입니다.");
+                }
                 // 더 구현해야함
+
             }
 
 
@@ -297,7 +314,8 @@ public class Kiosk2 {
     private static void delay(long millis) {
         try {
             Thread.sleep(millis);
-        } catch (InterruptedException ignored) {}
+        } catch (InterruptedException ignored) {
+        }
     } //delay
 
 } //Kiosk2 class
